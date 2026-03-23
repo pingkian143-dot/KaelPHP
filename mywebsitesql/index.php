3e40e741309062a6c5271091d9d8ab370d9f2f94
pingkian143-dot <pingkian143@gmail.com>
2026-03-23 18:48:12 +0800

    Add files via upload

---
+++ mywebsitesql/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <style>
+        body {
+            height: 100vh;
+        }
+    </style>
+</head>
+
+<body>
+
+<form>
+    <h2>Kieth Lawrence L. Nisnisan</h2>
+    <label>Username:</label><br>
+    <input type="text"><br><br>
+
+    <label>Password:</label><br>
+    <input type="password"><br><br>
+
+    <button type="submit">Login</button>
+</form>
+</body>
+</html>
